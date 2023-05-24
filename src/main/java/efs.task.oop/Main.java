c2f74151c0ba92fdad2be5edda825d58e9804444
Maks Toczek <makstorpl@gmail.com>
2023-05-24 02:28:55 +0200

    add ExtraordinaryVillager Class

--- src/main/java/efs.task.oop/Main.java
+++ src/main/java/efs.task.oop/Main.java
@@ -2,6 +2,18 @@ package efs.task.oop;
 
 public class Main {
     public static void main(String[] args) {
+        Villager kashya = new Villager("Kashya", 30);
+        Villager akara = new ExtraordinaryVillager("Akara", 40, ExtraordinaryVillager.Skill.IDENTIFY);
+        Villager gheed = new Villager("Gheed", 50);
+        Villager deckard = new ExtraordinaryVillager("Deckard Cain", 85, ExtraordinaryVillager.Skill.SHELTER);
+        Villager warriv = new Villager("Warriv", 35);
+        Villager flawia = new Villager("Flawia", 25);
 
+        kashya.sayHello();
+        akara.sayHello();
+        gheed.sayHello();
+        deckard.sayHello();
+        warriv.sayHello();
+        flawia.sayHello();
     }
 }
--- src/main/java/efs.task.oop/Villager.java
+++ src/main/java/efs.task.oop/Villager.java
@@ -1,4 +1,25 @@
 package efs.task.oop;
 
 public class Villager {
+    protected String name;
+    protected int age;
+    protected int health;
+
+    public Villager(String name, int age){
+        this.name = name;
+        this.age = age;
+        this.health = 100;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void sayHello(){
+        System.out.println("Greetings traveler... I'm " + getName() + " and I'm " + getAge() + " years old.");
+    }
 }
